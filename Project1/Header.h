23f38d9196931522caf62904028d634258e151d8
Алексей Ермолаев <asvaltgovno@gmail.com>
2022-11-07 10:23:34 +0300

    fd

--- Project1/Header.h
+++ Project1/Header.h
@@ -2,10 +2,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
 #include <stdio.h>
+#include <string.h>
+
 
 typedef struct ANKETA {
 	char* Name;
 	char* SecondName;
 	char* Surname;
-	int age;
+	char* Age;
 } anketa;
+typedef struct Array {
+	int size;
+	anketa* Peoples;
+}Array;
--- Project1/Source.c
+++ Project1/Source.c
@@ -1,27 +1,53 @@
 #include"Header.h"
-INT WINAPI WinMain(HINSTANCE histance, HINSTANCE hPrevInstance, LPSTR lpCMDline, int nCmdShow) {
-	FILE* pFile = fopen("1.txt", "r");
+Array GetData() {
+	FILE* pFile = fopen("data.csv", "r");
 	fseek(pFile, 0, SEEK_END);
 	int size = ftell(pFile);
+	fseek(pFile, 0, SEEK_SET);
 	fclose(pFile);
-	HANDLE file = CreateFile(L"1.txt", GENERIC_READ, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	HANDLE file = CreateFile(L"data.csv", GENERIC_READ, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 	DWORD bytes;
 	char* text = calloc(size, 1);
 	ReadFile(file, text, size, &bytes, NULL);
 	CloseHandle(file);
-	int indeX = 0;
 	size = 0;
 	for (size_t i = 0; i < bytes; i++)
 	{
-		if (text[i] == '\n') {
-			break;
+		if ((text[i] == '\n') || (i == bytes - 1)) {
+			size++;
 		}
-		size = i;
-		indeX = i;
 	}
-	char* line = calloc(size, 1);
-	for (size_t i = indeX-size; i < size; i++)
+	
+	anketa* peoples = calloc(size, sizeof(anketa));
+	char** lines = calloc(size, 1);
+	int line = 0;
+	int charindex = 0;
+	for (size_t i = 0; i < size; i++)
 	{
-		line[i] = text[i];
+		lines[i] = calloc(100, 1);
 	}
+	for (size_t i = 0; i < bytes; i++)
+	{
+		lines[line][charindex] = text[i];
+		charindex++;
+		if ((text[i + 1] == '\r') && (text[i + 2] == '\n')) {
+			line++;
+			i += 2;
+			charindex = 0;
+		}
+	}
+	for (size_t i = 0; i < size; i++)
+	{
+		char* buf=strtok(lines[i], ";");
+		peoples[i].Surname = buf;
+		buf= strtok(NULL, ";");
+		peoples[i].Name = buf;
+		buf = strtok(NULL, ";");
+		peoples[i].SecondName = buf;
+		buf = strtok(NULL, ";");
+		peoples[i].Age = buf;
+		
+	}
+	Array array = { size,peoples };
+	return array;
 }
